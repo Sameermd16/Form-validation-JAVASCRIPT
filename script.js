74a7e6d9d0f573430d71c7369e54c80e30f8951b
sameermd16 <sameerthemc@gmail.com>
2023-02-08 01:44:27 +0530

    final code

--- script.js
+++ script.js
@@ -22,24 +22,47 @@ function inputChecks() {
     }else {
         success(username);
     }
-    
+    if (emailValue === ""){
+        error(email, "Email cannot be blank");
+    }
+    else if (!isEmail(emailValue)){
+        error(email, "Email is invalid")
+    }else {
+        success(email);
+    }
+    if (passwordValue === ""){
+        error(password, "Password cannot be blank");
+    }else {
+        success(password);
+    }
+    if (confirmPasswordValue === ""){
+        error(confirmPassword, "Confirm  password cannot be blank")
+    }else if (passwordValue !== confirmPasswordValue){
+        error(confirmPassword, "Passwords does not match");
+    }else {
+        success(confirmPassword)
+    }
 }
 
 
 function error(input, message){
     const formControl = input.parentElement;
     const small = formControl.querySelector("small");
-    formControl.classList.add("error");
-    // formControl.className = "form-control error";
+    // formControl.classList.add("error");
+    formControl.className = "form-control error";
     small.innerText = message;
 }
 function success(input){
     const formControl = input.parentElement;
-    formControl.classList.add("success");
-    // formControl.className = "form-control success";
+    // formControl.classList.add("success");
+    formControl.className = "form-control success";
 }
 
-function isValid(parameter){
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+// function isValid(email){
+//     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 	return re.test(String(email).toLowerCase());
+// }
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
